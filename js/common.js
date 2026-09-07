e2ead65432c64e0f03d6adc9bad37d9f0ccbb6fe
SUNG SEON HYEOK <steve5535@naver.com>
2026-09-07 20:36:18 +0900

    2026-09-07

--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:8080";
+const API_BASE_URL = "https://past-workflow-forbes-teachers.trycloudflare.com";
 
 async function apiFetch(path, options = {}) {
     const config = {
