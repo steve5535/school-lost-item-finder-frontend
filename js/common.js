7f9e1270b46d8cc523e3b9eb35ec18857b1cb209
SUNG SEON HYEOK <steve5535@naver.com>
2026-09-10 22:43:32 +0900

    url 변경

--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://losses-able-licensed-doors.trycloudflare.com";
+const API_BASE_URL = "https://ball-sunrise-punch-contribute.trycloudflare.com";
 
 async function apiFetch(path, options = {}) {
     const isFormData = options.body instanceof FormData;
