5e35b8679694fdfa0927f038d36a88cb0f71525d
SUNG SEON HYEOK <steve5535@naver.com>
2026-09-10 18:14:19 +0900

    url변경

--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://lexmark-river-listings-mug.trycloudflare.com";
+const API_BASE_URL = "https://field-prospects-candidate-flashers.trycloudflare.com";
 
 async function apiFetch(path, options = {}) {
     const isFormData = options.body instanceof FormData;
